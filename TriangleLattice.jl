f6ab8d18fd16e038378a47c18c241185a07f7824
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2024-11-09 20:09:11 +0900

    Set Plot Size

--- TriangleLattice.jl
+++ TriangleLattice.jl
@@ -16,15 +16,17 @@ m = 6.63e-3
 
 f_ar(r) = LennardJones.force(r,ϵ,σ)
 #---------------------------------------------------------------------------------------------------------------------------
-la = 0.4 # 格子定数
-row = 4::Int64
-col = 6::Int64
+λ = 0.4 # 格子定数
+row = 10::Int64
+col = 10::Int64
 #---------------------------------------------------------------------------------------------------------------------------
-as = vec([Atom([x,y],[0.0,0.0],m) for x=0.0:la:la*(col-1),y=0.0:la*√3:la*√3*(row-1)])
-bs = vec([Atom([x,y],[0.0,0.0],m) for x=la/2:la:la*(col-1),y=la*√3/2:la*√3:la*√3*(row-1)])
+#as = vec([Atom([x,y],[0.0,0.0],m) for x=0.0:la:la*(col-1),y=0.0:la*√3:la*√3*(row-1)])
+as = vec([Atom([(x-1)*λ,(y-1)*√3λ],[0.0,0.0],m) for x=1:col, y=1:div(row,2)+mod(row,2)])
+#bs = vec([Atom([x,y],[0.0,0.0],m) for x=la/2:la:la*(col-1),y=la*√3/2:la*√3:la*√3*(row-1)])
+bs = vec([Atom([(x-1/2)*λ,(y-1/2)*√3λ],[0.0,0.0],m) for x=1:col-1, y=1:div(row,2)])
 append!(as,bs)
 Δt = 1e-6
-n = 1e4
+n = 1e6
 samp = 1000
 
 anim = Animation()
@@ -33,7 +35,7 @@ anim = Animation()
         xs = [a.x[1] for a in as]
         ys = [a.x[2] for a in as]
         time = i/samp
-        plt = plot(xs,ys,st=:scatter,xlims=(-0.2,2.2),ylims=(-0.2,2.2),label="",aspect_ratio=:equal,title="$time[ns]")
+        plt = plot(xs,ys,st=:scatter,xlims=(-λ/2,(col-1/2)*λ),ylims=(-λ/2,(row-1/2)*√3/2*λ),label="",aspect_ratio=:equal,title="$time[ns]")
         frame(anim,plt)
     end
     accs = calc_acc(as,f_ar)
